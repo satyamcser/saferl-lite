abaf108e677d72a5027644adf5b7fe9137293fac
Satyam Mishra <satyam.mishra@visionmentorsvn.com>
2025-06-18 00:36:30 +0700

    release: bump to v0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="saferl-lite",
-    version="0.1.0",
+    version="0.1.2",
     author="Satyam Mishra",
     author_email="satyam@example.com",
     description="A lightweight, explainable, and constrained reinforcement learning toolkit.",
